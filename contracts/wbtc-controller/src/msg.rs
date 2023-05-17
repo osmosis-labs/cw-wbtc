8bc0c3fc9858d91082ac0811d8307ed9a52d6a06
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-05-17 14:34:19 +0700

    add inline comments for msgs

--- contracts/wbtc-controller/src/msg.rs
+++ contracts/wbtc-controller/src/msg.rs
@@ -7,9 +7,11 @@ use crate::tokenfactory::{
     mint::{MintRequest, MintRequestStatus, MintRequestWithHash},
 };
 
-/// Message type for `instantiate` entry_point
 #[cw_serde]
 pub struct InstantiateMsg {
+    /// Address of the owner of the contract.
+    /// The owner can set the custodian and add/remove merchants.
+    /// Ownership can be transferred to another address.
     pub owner: String,
 
     /// Subdenom of the token that will be created on behalf of this contract
@@ -17,117 +19,167 @@ pub struct InstantiateMsg {
     pub subdenom: String,
 }
 
-/// Message type for `execute` entry_point
 #[cw_serde]
 pub enum ExecuteMsg {
-    TransferOwnership {
-        new_owner_address: String,
-    },
-    SetCustodian {
-        address: String,
-    },
-    AddMerchant {
-        address: String,
-    },
-    RemoveMerchant {
-        address: String,
-    },
+    /// Transfer ownership of the contract to another address.
+    TransferOwnership { new_owner_address: String },
+
+    /// Set custodian address.
+    SetCustodian { address: String },
+
+    /// Add merchant address.
+    AddMerchant { address: String },
+
+    /// Remove merchant address.
+    RemoveMerchant { address: String },
+
     /// Set custodian BTC deposit address of the specified merchant
     SetCustodianDepositAddress {
         merchant: String,
         deposit_address: String,
     },
-    SetMerchantDepositAddress {
-        deposit_address: String,
-    },
+
+    /// Set merchant BTC deposit address. Message sender must be a merchant.
+    /// This deposit address will be associated with message sender.
+    SetMerchantDepositAddress { deposit_address: String },
+
+    /// Issue request to mint tokens.
+    /// Only merchants can issue mint requests.
+    /// The request needs to be approved by the custodian in order to mint tokens.
     IssueMintRequest {
         amount: Uint128,
         tx_id: String,
         deposit_address: String,
     },
-    CancelMintRequest {
-        request_hash: String,
-    },
-    ApproveMintRequest {
-        request_hash: String,
-    },
-    RejectMintRequest {
-        request_hash: String,
-    },
-    Burn {
-        amount: Uint128,
-    },
-    ConfirmBurnRequest {
-        request_hash: String,
-        tx_id: String,
-    },
-    SetDenomMetadata {
-        metadata: Metadata,
-    },
+
+    /// Cancel mint request. Message sender must be the requester.
+    CancelMintRequest { request_hash: String },
+
+    /// Approve mint request. Message sender must be the custodian.
+    /// The custodian will verify the BTC deposit if it's matched with the requested amount.
+    /// If approved, the tokens will be minted to requester address.
+    ApproveMintRequest { request_hash: String },
+
+    /// Reject mint request. Message sender must be the custodian.
+    RejectMintRequest { request_hash: String },
+
+    /// Burn tokens. Message sender must be merchant.
+    /// Funds attached with execute message must match the amount of tokens and denom to be burned.
+    /// The tokens will be burned immediately and the BTC will be sent from custodian to the merchant's deposit address.
+    Burn { amount: Uint128 },
+
+    /// Only custodian can execute this message.
+    /// Once the custodian has sent the BTC to the merchant's deposit address, the custodian can confirm the burn request.
+    ConfirmBurnRequest { request_hash: String, tx_id: String },
+
+    /// Set denom metadata. Message sender must be the owner.
+    SetDenomMetadata { metadata: Metadata },
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
+    /// Get mint request by nonce.
     #[returns(GetMintRequestByNonceResponse)]
     GetMintRequestByNonce { nonce: Uint128 },
 
+    /// Get mint request by hash.
     #[returns(GetMintRequestByHashResponse)]
     GetMintRequestByHash { request_hash: String },
 
+    /// Count total mint requests.
     #[returns(GetMintRequestsCountResponse)]
     GetMintRequestsCount {},
 
+    /// List mint requests with filter and pagination.
+    /// Ordered by nonce.
     #[returns(ListMintRequestsResponse)]
     ListMintRequests {
+        /// Maximum number of results to return.
+        /// If not specified, default to 10.
+        /// Max limit is 100.
         limit: Option<u32>,
+
+        /// Start after the specified nonce.
         start_after_nonce: Option<Uint128>,
+
+        /// Filter by status.
+        /// If not specified, default to all statuses.
         status: Option<MintRequestStatus>,
     },
 
+    /// Get burn request by nonce.
     #[returns(GetBurnRequestByNonceResponse)]
     GetBurnRequestByNonce { nonce: Uint128 },
 
+    /// Get burn request by hash.
     #[returns(GetBurnRequestByHashResponse)]
     GetBurnRequestByHash { request_hash: String },
 
+    /// Count total burn requests.
     #[returns(GetBurnRequestsCountResponse)]
     GetBurnRequestsCount {},
 
+    /// List burn requests with filter and pagination.
+    /// Ordered by nonce.
     #[returns(ListBurnRequestsResponse)]
     ListBurnRequests {
+        /// Maximum number of results to return.
+        /// If not specified, default to 10.
+        /// Max limit is 100.
         limit: Option<u32>,
+
+        /// Start after the specified nonce.
         start_after_nonce: Option<Uint128>,
+
+        /// Filter by status.
+        /// If not specified, default to all statuses.
         status: Option<BurnRequestStatus>,
     },
 
+    /// Get token denom associated with this contract.
     #[returns(GetTokenDenomResponse)]
     GetTokenDenom {},
 
+    /// Check if the specified address is a merchant.
     #[returns(IsMerchantResponse)]
     IsMerchant { address: String },
 
+    /// List merchants with pagination.
+    /// Ordered by address.
     #[returns(ListMerchantsResponse)]
     ListMerchants {
+        /// Maximum number of results to return.
+        /// If not specified, default to 10.
+        /// Max limit is 100.
         limit: Option<u32>,
+
+        /// Start after the specified address.
+        /// If not specified, default to the first address.
         start_after: Option<String>,
     },
 
+    /// Check if the specified address is a custodian.
     #[returns(IsCustodianResponse)]
     IsCustodian { address: String },
 
+    /// Get custodian address.
     #[returns(GetCustodianResponse)]
     GetCustodian {},
 
+    /// Get owner address.
     #[returns(GetOwnerResponse)]
     GetOwner {},
 
+    /// Check if the specified address is the owner.
     #[returns(IsOwnerResponse)]
     IsOwner { address: String },
 
+    /// Get custodian deposit address of the specified merchant.
     #[returns(GetCustodianDepositAddressResponse)]
     GetCustodianDepositAddress { merchant: String },
 
+    /// Get merchant deposit address of the specified merchant.
     #[returns(GetMerchantDepositAddressResponse)]
     GetMerchantDepositAddress { merchant: String },
 }
