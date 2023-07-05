5c055e5bd7c2745a308d0893d4de3b5029c715fe
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-07-05 15:19:37 +0700

    ignore codecov for bin

--- contracts/wbtc-controller/src/bin/schema.rs
+++ contracts/wbtc-controller/src/bin/schema.rs
@@ -2,6 +2,7 @@ use cosmwasm_schema::write_api;
 
 use wbtc_controller::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
+#[cfg(not(tarpaulin_include))]
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
