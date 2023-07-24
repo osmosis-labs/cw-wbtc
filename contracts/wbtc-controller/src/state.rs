3ce55156be1f746fe782e09ea8c1e2f0b7ac4213
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-07-24 16:10:29 +0200

    move token's related storage entries to state.rs

--- contracts/wbtc-controller/src/state.rs
+++ contracts/wbtc-controller/src/state.rs
@@ -8,3 +8,9 @@ pub const MEMBER_MANAGER: Item<Addr> = Item::new("member_manager");
 /// Merchants storage is a map of merchant addresses to empty values
 /// This makes it efficient to check if a merchant exists while not storing any data as value
 pub const MERCHANTS: Map<Addr, ()> = Map::new("merchants");
+
+/// Token denom storage.
+pub const TOKEN_DENOM: Item<String> = Item::new("token_denom");
+
+/// Pause status storage.
+pub const IS_PAUSED: Item<bool> = Item::new("is_paused");
