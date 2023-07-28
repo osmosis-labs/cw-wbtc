120f038b41f3b9df6531ace42d7c62d1bf02b6d4
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2023-07-28 10:38:28 +0700

    add tests for transfer & claim process

--- contracts/wbtc-controller/src/auth/governor.rs
+++ contracts/wbtc-controller/src/auth/governor.rs
@@ -6,6 +6,7 @@ use crate::{attrs::action_attrs, state::auth::GOVERNOR, ContractError};
 
 use super::{allow_only, has_no_priviledged_role, Role};
 
+/// State of the governor to be stored in the contract storage
 #[cw_serde]
 pub enum GovernorState {
     Claimed(Addr),
@@ -74,7 +75,7 @@ pub fn claim_governorship(deps: DepsMut, info: MessageInfo) -> Result<Response, 
     let governor = GOVERNOR.load(deps.storage)?;
     let candidate = governor.candidate().ok_or(ContractError::Unauthorized {})?;
 
-    ensure!(info.sender != candidate, ContractError::Unauthorized {});
+    ensure!(info.sender == candidate, ContractError::Unauthorized {});
     has_no_priviledged_role(deps.as_ref(), &candidate)?;
 
     GOVERNOR.save(deps.storage, &GovernorState::claimed(info.sender))?;
@@ -119,6 +120,8 @@ pub fn get_governor_candidate(deps: Deps) -> Result<Option<Addr>, StdError> {
 mod tests {
     use cosmwasm_std::testing::{mock_dependencies, mock_info};
 
+    use crate::auth::member_manager;
+
     use super::*;
 
     #[test]
@@ -127,9 +130,9 @@ mod tests {
         let governor_address = "osmo1governor";
         let non_governor_address = "osmo1nongovernor";
         let new_governor_address = "osmo1newgovernor";
+        let member_manager_address = "osmo1membermanager";
 
         // check before set will fail
-
         assert!(!is_governor(deps.as_ref(), &Addr::unchecked(governor_address)).unwrap(),);
 
         let err = get_governor(deps.as_ref()).unwrap_err();
@@ -137,6 +140,13 @@ mod tests {
 
         // initialize governor
         initialize_governor(deps.as_mut(), governor_address).unwrap();
+
+        member_manager::set_member_manager(
+            deps.as_mut(),
+            &mock_info(governor_address, &[]),
+            member_manager_address,
+        )
+        .unwrap();
 
         // check after set will pass
         assert_eq!(get_governor(deps.as_ref()).unwrap(), governor_address);
@@ -156,6 +166,20 @@ mod tests {
         assert!(is_governor(deps.as_ref(), &Addr::unchecked(governor_address)).unwrap(),);
         assert!(!is_governor(deps.as_ref(), &Addr::unchecked(non_governor_address)).unwrap(),);
 
+        // transfer governor right should fail if called by governor but the candidate address is a priviledged address
+        let err = transfer_governorship(
+            deps.as_mut(),
+            &mock_info(governor_address, &[]),
+            member_manager_address,
+        )
+        .unwrap_err();
+        assert_eq!(
+            err,
+            ContractError::AlreadyHasPriviledgedRole {
+                address: member_manager_address.to_string()
+            }
+        );
+
         // transfer governor right should pass if called by governor
         assert_eq!(
             transfer_governorship(
@@ -167,6 +191,65 @@ mod tests {
             .attributes,
             vec![
                 attr("action", "transfer_governorship"),
+                attr("address", new_governor_address)
+            ]
+        );
+
+        // governor remain unchanged until the new governor claims the governorship
+        assert_eq!(get_governor(deps.as_ref()).unwrap(), governor_address);
+        assert_eq!(
+            get_governor_candidate(deps.as_ref()).unwrap(),
+            Some(new_governor_address).map(Addr::unchecked)
+        );
+
+        assert!(is_governor(deps.as_ref(), &Addr::unchecked(governor_address)).unwrap());
+        assert!(!is_governor_candidate(deps.as_ref(), &Addr::unchecked(governor_address)).unwrap());
+
+        assert!(!is_governor(deps.as_ref(), &Addr::unchecked(new_governor_address)).unwrap());
+        assert!(
+            is_governor_candidate(deps.as_ref(), &Addr::unchecked(new_governor_address)).unwrap()
+        );
+
+        // claim governorship should fail if not called by governor candidate
+        let err =
+            claim_governorship(deps.as_mut(), mock_info(non_governor_address, &[])).unwrap_err();
+        assert_eq!(err, ContractError::Unauthorized {});
+
+        let err = claim_governorship(deps.as_mut(), mock_info(governor_address, &[])).unwrap_err();
+        assert_eq!(err, ContractError::Unauthorized {});
+
+        // claim governorship should fail if called by governor candidate but the candidate address is a priviledged address
+        member_manager::set_member_manager(
+            deps.as_mut(),
+            &mock_info(governor_address, &[]),
+            new_governor_address,
+        )
+        .unwrap();
+
+        let err =
+            claim_governorship(deps.as_mut(), mock_info(new_governor_address, &[])).unwrap_err();
+        assert_eq!(
+            err,
+            ContractError::AlreadyHasPriviledgedRole {
+                address: new_governor_address.to_string()
+            }
+        );
+
+        // remove member manager role from the candidate address
+        member_manager::set_member_manager(
+            deps.as_mut(),
+            &mock_info(governor_address, &[]),
+            member_manager_address,
+        )
+        .unwrap();
+
+        // claim governorship should pass if called by governor candidate
+        assert_eq!(
+            claim_governorship(deps.as_mut(), mock_info(new_governor_address, &[]))
+                .unwrap()
+                .attributes,
+            vec![
+                attr("action", "claim_governorship"),
                 attr("address", new_governor_address)
             ]
         );
